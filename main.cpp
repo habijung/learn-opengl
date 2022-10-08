4851730d2c967ad1b79128065eae6c88d57dbe19
habi <habijung0@gmail.com>
2022-10-08 23:55:25 +0900

    Hello Triangle 준비
    - Hello Window 결과를 main.cpp로 복사
    - CMakeLists에서 실행 코드 수정

---
+++ main.cpp
@@ -0,0 +1,70 @@
+//
+// Created by habi on 10/08/2022.
+//
+#include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+using namespace std;
+
+
+/* Prototypes */
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+void processInput(GLFWwindow *window);
+
+
+/* Main */
+int main() {
+    cout << "Run Main()" << endl;
+
+    /* GLFW 초기화 */
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    /* Window object 생성 */
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello Triangle", NULL, NULL);
+    if (window == NULL) {
+        cout << "Failed to creat GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    /* Initialize GLAD */
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    /* Render Loop */
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
+
+
+/* Functions */
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
