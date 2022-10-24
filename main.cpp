9fb844c005144474a26839a0f3e2088fe14d1bd3
habi <habijung0@gmail.com>
2022-10-24 16:36:40 +0900

    Top Left 점의 색상을 흰색 (1.0, 1.0, 1.0)으로 변경

--- main.cpp
+++ main.cpp
@@ -116,7 +116,7 @@ int main() {
             0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,         // top right
             0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,      // bottom right
             -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  // bottom left
-            -0.5f, 0.5f, 0.0f, 0.1f, 0.2f, 0.3f    // top left
+            -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f    // top left
     };
     unsigned int indices[] = {
             0, 1, 3,   // first triangle
