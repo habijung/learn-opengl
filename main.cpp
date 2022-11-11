1d8c005d49b468c68ef2852c677758ef29de7927
habi <habijung0@gmail.com>
2022-11-11 11:28:45 +0900

    STB 라이브러리 세팅 및 texture binding

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 #include "GLFW/glfw3.h"
 #include "../../shaders/1.Getting-Started/shader.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+
+#include "stb_image.h"
+
 using namespace std;
 
 
@@ -68,6 +72,11 @@ int main() {
             0.5f, 1.0f,     // top-center corner
     };
 
+    /* Texture settings */
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
     /* Textures option settings */
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
@@ -79,6 +88,18 @@ int main() {
     // 축소 & 확대에 관한 설정
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    /* STB settings */
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("../img/container.jpg", &width, &height, &nrChannels, 0);
+
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        cout << "Failed to load texture" << endl;
+    }
+    stbi_image_free(data); // 텍스처와 밉맵 생성 완료 후 이미지 메모리 해제
 
     /* Set up buffers */
     unsigned int VBO, VAO, EBO;
