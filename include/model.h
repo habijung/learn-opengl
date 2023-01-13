ba9345d50293467441fa18e88dbd8a891df5c3c8
habi <habijung0@gmail.com>
2023-01-13 23:30:54 +0900

    feat: 파일에서 texture를 읽는 함수 구현
    - Learn OpenGL에서는 format 초기화가 없어 추가함

--- include/model.h
+++ include/model.h
@@ -10,8 +10,13 @@
 #include "assimp/Importer.hpp"
 #include "assimp/scene.h"
 #include "assimp/postprocess.h"
+#include "stb_image.h"
 #include "shaders/shader.h"
 #include "mesh.h"
+
+
+/* Prototypes */
+unsigned int TextureFromFile(const char *path, const string &directory, bool gamma = false);
 
 
 class Model {
@@ -143,5 +148,49 @@ private:
         return textures;
     }
 };
+
+
+/* Functions */
+unsigned int TextureFromFile(const char *path, const string &directory, bool gamma) {
+    string filename = string(path);
+    filename = directory + '/' + filename;
+
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
+
+    if (data) {
+        GLenum format;
+
+        if (nrComponents == 1) {
+            format = GL_RED;
+        } else if (nrComponents == 3) {
+            format = GL_RGB;
+        } else if (nrComponents == 4) {
+            format = GL_RGBA;
+        } else {
+            // format 변수의 initialize를 위해 추가
+            format = GL_ZERO;
+        }
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    } else {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+
+    return textureID;
+}
 
 #endif //MODEL_H
