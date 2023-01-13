081cc66cd27a6ed6ab565b5683e8567fe5dbf3d0
habi <habijung0@gmail.com>
2023-01-13 14:48:39 +0900

    feat: Assimp Importer로 모델 읽기 디자인

--- include/model.h
+++ include/model.h
@@ -7,7 +7,11 @@
 
 #include <vector>
 #include <string>
+#include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
 #include "shaders/shader.h"
+#include "mesh.h"
 
 
 class Model {
@@ -16,16 +20,41 @@ public:
         loadModel(path);
     }
 
-    void Draw(Shader &shader);
+    void Draw(Shader &shader) {
+        for (unsigned int i = 0; i < meshes.size(); i++) {
+            meshes[i].Draw(shader);
+        }
+    }
 
 private:
     // Model data
     std::vector<Mesh> meshes;
     std::string directory;
 
-    void loadModel(std::string path);
+    void loadModel(std::string path) {
+        Assimp::Importer importer;
+        const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
 
-    void processNode(aiNode *node, const aiScene *scene);
+        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+            std::cout << "ERROR::ASSIMP::" << importer.GetErrorString() << endl;
+        }
+        directory = path.substr(0, path.find_last_of('/'));
+        processNode(scene->mRootNode, scene);
+    }
+
+    // scene의 모든 node를 처리하기 위해 recursive로 구현
+    void processNode(aiNode *node, const aiScene *scene) {
+        // Process all the node's meshes (if any)
+        for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+            aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+            meshes.push_back(processMesh(mesh, scene));
+        }
+
+        // Then do the same for each of its children
+        for (unsigned int i = 0; i < node->mNumChildren; i++) {
+            processNode(node->mChildren[i], scene);
+        }
+    }
 
     Mesh processMesh(aiMesh *mesh, const aiScene *scene);
 
