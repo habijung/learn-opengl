936ef663570e127150dfa701519cdab0fee553c1
habi <habijung0@gmail.com>
2023-02-03 11:54:23 +0900

    feat: Normal 정보로 diffuse color 계산

--- include/shaders/lighting/object.frag
+++ include/shaders/lighting/object.frag
@@ -1,13 +1,25 @@
 #version 330 core
+
+in vec3 FragPos;
+in vec3 Normal;
 out vec4 FragColor;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
+uniform vec3 lightPos;
 
 void main() {
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
 
-    vec3 result = ambient * objectColor;
+    // 조명의 방향 계산
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+
+    // 조명의 각도 계산
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    vec3 result = (ambient + diffuse) * objectColor;
     FragColor = vec4(result, 1.0);
 }
--- src/2.2.Basic-Lighting.cpp
+++ src/2.2.Basic-Lighting.cpp
@@ -104,11 +104,13 @@ int main() {
     glGenVertexArrays(1, &objectVAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_normal), cube_normal, GL_STATIC_DRAW);
 
     glBindVertexArray(objectVAO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // Generate lighting VAO
     unsigned int lightingVAO;
@@ -118,7 +120,7 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     glBindVertexArray(lightingVAO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
 
     /* Set up random cube position */
@@ -149,6 +151,7 @@ int main() {
         objectShader.use();
         objectShader.setVec3("objectColor", vec3(1.0f, 0.5f, 0.31f));
         objectShader.setVec3("lightColor", vec3(1.0f, 1.0f, 1.0f));
+        objectShader.setVec3("lightPos", lightPos);
 
         // Transformations
         mat4 projection = perspective(radians(camera.Zoom), (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
